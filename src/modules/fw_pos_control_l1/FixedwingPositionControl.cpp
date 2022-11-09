49463232bb3344df114a293531a08c3c3f69bc0f
Silvan Fuhrer <silvan@auterion.com>
2022-11-09 16:35:45 +0100

    FW PosC: remove hack to force _landed to false if (not)launch_detected, as it is now handles in land detector
    Signed-off-by: Silvan Fuhrer <silvan@auterion.com>

--- src/modules/fw_pos_control_l1/FixedwingPositionControl.cpp
+++ src/modules/fw_pos_control_l1/FixedwingPositionControl.cpp
@@ -1583,8 +1583,6 @@ FixedwingPositionControl::control_auto_takeoff(const hrt_abstime &now, const flo
 				_att_sp.roll_body = _l1_control.get_roll_setpoint();
 			}
 
-			/* Select throttle: only in LAUNCHDETECTION_RES_DETECTED_ENABLEMOTORS we want to use
-			 * full throttle, otherwise we use idle throttle */
 			const float max_takeoff_throttle = (_launchDetector.getLaunchDetected() < launch_detection_status_s::STATE_FLYING) ?
 							   _param_fw_thr_idle.get() : _param_fw_thr_max.get();
 
@@ -2259,8 +2257,6 @@ FixedwingPositionControl::Run()
 
 			if (_vehicle_land_detected_sub.update(&vehicle_land_detected)) {
 				_landed = vehicle_land_detected.landed;
-
-				if (_launch_detected) {_landed = false;}
 			}
 		}
 
